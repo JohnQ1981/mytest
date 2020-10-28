7c7b46441bcb7edca19e3c5b750587b2a49207d6
JohnQ1981 <69947888+JohnQ1981@users.noreply.github.com>
2020-10-28 09:32:21 -0500

    Update Test.java

--- src/mytest/Test.java
+++ src/mytest/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Trying Git");
 		System.out.println("Done in github");
+		for(int i=0; i<3;i++){
+			System.out.println("Hello World");
+		}
 
 	}
 
